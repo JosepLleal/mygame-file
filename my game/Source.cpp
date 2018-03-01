c1f43bf453f7e50c4145636a6124f9d14612ec77
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2018-03-01 14:30:01 +0100

    Moving with keys
    Now the square moves if I press a key.

--- my game/Source.cpp
+++ my game/Source.cpp
@@ -6,31 +6,67 @@
 
 
 int main(int argc, char* argv[]) {
+		
+	bool b[4] = { 0, 0, 0, 0 };
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window *window = SDL_CreateWindow("Homework", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, SDL_WINDOW_SHOWN);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+	//SDL_SetRenderDrawColor(renderer, 0, 150, 255, 255);
+	//SDL_RenderClear(renderer);
+
+	//Creating square
+	SDL_Rect rect;
+	rect.w = 120;
+	rect.h = 120;
+	rect.x = 440;
+	rect.y = 340;
 
 	
-
-		SDL_Init(SDL_INIT_VIDEO);
-
-		SDL_Window *window = SDL_CreateWindow("Homework", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, SDL_WINDOW_SHOWN);
-		SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
-
-		//SDL_SetRenderDrawColor(renderer, 0, 150, 255, 255);
-		//SDL_RenderClear(renderer);
-
-		//Creating square
-		SDL_Rect rect;
-		rect.w = 120;
-		rect.h = 120;
-		rect.x = 440;
-		rect.y = 340;
-
-		int a = 1; 
-		int b = 1;
-
-		bool isRunning = true;
-		SDL_Event event;
+	bool isRunning = true;
+	SDL_Event event;
+	
 			
 	while (isRunning) {
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				isRunning = false;
+			}
+			switch (event.type) {
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym) {
+				case SDLK_LEFT:
+					rect.x -= 10;
+					break;
+				case SDLK_RIGHT:
+					rect.x += 10;
+					break;
+				case SDLK_UP:
+					rect.y -= 10;
+					break;
+				case SDLK_DOWN:
+					rect.y += 10;
+					break;
+
+				}
+				
+			}
+		}
+		if (rect.x < 0) {
+			rect.x = 0;
+		}
+		else if (rect.x > 1000 - rect.w) {
+			rect.x = 1000 - rect.w;
+		}
+		if (rect.y < 0) {
+			rect.y = 0;
+		}
+		else if (rect.y > 800 -rect.h) {
+			rect.y = 800 - rect.h;
+		}
+
 		//Refreshing color of square
 		SDL_SetRenderDrawColor(renderer, 0, 150, 255, 255);
 		SDL_RenderClear(renderer);
@@ -38,34 +74,9 @@ int main(int argc, char* argv[]) {
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 		SDL_RenderFillRect(renderer, &rect);
 
-		//Square bounding
-		if (rect.x >= 1000 - rect.w) {
-			a=-1;
-		}
-		else if (rect.y >=800 - rect.h) {
-			b=-1;
-		}
-		else if (rect.x <= 0) {
-			a = 1;
-		}
-		else if (rect.y <= 0) {
-			b = 1;
-		}
-
-		rect.x += a;
-		rect.y += b;
-	
 		SDL_RenderPresent(renderer);
 		//Reducing speed
 		SDL_Delay(3);
-
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
-				isRunning = false;
-			}
-		}
-
-
 
 	}
 	
