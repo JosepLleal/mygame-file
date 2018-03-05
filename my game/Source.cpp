adef0040021547209763e14327edd13339ffe437
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2018-03-05 18:50:31 +0100

    Shooting bullet version
    Bullet code added.

--- my game/Source.cpp
+++ my game/Source.cpp
@@ -22,6 +22,14 @@ int main(int argc, char* argv[]) {
 	rect.x = 440;
 	rect.y = 340;
 
+	//Creating bullet
+	SDL_Rect bullet;
+	bullet.w = 50;
+	bullet.h = 5;
+	
+
+
+
 	
 	bool isRunning = true;
 	SDL_Event event;
@@ -49,11 +57,21 @@ int main(int argc, char* argv[]) {
 				case SDLK_DOWN:
 					rect.y += 10;
 					break;
-
+				//shooting bullet
+				case SDLK_SPACE:
+					bullet.x = rect.x+120;
+					bullet.y = rect.y+60;
+					break;
 				}
+				
 				
 			}
 		}
+
+		bullet.x++;
+		bullet.x++;
+		bullet.x++;
+
 		//limits of movement
 		if (rect.x < 0) {
 			rect.x = 0;
@@ -74,6 +92,11 @@ int main(int argc, char* argv[]) {
 
 		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 		SDL_RenderFillRect(renderer, &rect);
+
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderFillRect(renderer, &bullet);
+
+		
 
 		SDL_RenderPresent(renderer);
 		//Reducing speed
